eca9f546439fa4d75ab9e1c81697b63e6ed97cfc
Necnion8 <necnionch@gmail.com>
2022-12-02 13:01:10 +0900

    Edit: ChestShop v3.12.0 以上の互換に変更

--- src/main/java/com/gmail/necnionch/myplugin/simplechestshopgui/bukkit/util/ShopUtil.java
+++ src/main/java/com/gmail/necnionch/myplugin/simplechestshopgui/bukkit/util/ShopUtil.java
@@ -1,6 +1,9 @@
 package com.gmail.necnionch.myplugin.simplechestshopgui.bukkit.util;
 
 import com.Acrobot.Breeze.Utils.PriceUtil;
+import com.Acrobot.Breeze.Utils.QuantityUtil;
+import com.Acrobot.Breeze.Utils.StringUtil;
+import org.bukkit.block.Sign;
 
 public class ShopUtil {
 
@@ -41,5 +44,56 @@ public class ShopUtil {
         return tmp;
     }
 
+
+    public static String getOwner(Sign sign) {
+        return getOwner(sign.getLines());
+    }
+
+    public static String getOwner(String[] lines) {
+        return StringUtil.stripColourCodes(strip(StringUtil.stripColourCodes(lines[0])));
+    }
+
+    public static String getQuantityLine(String[] lines) throws IllegalArgumentException {
+        return lines.length > 1 ? strip(StringUtil.stripColourCodes(lines[1])) : "";
+    }
+
+    public static int getQuantity(Sign sign) throws IllegalArgumentException {
+        return getQuantity(sign.getLines());
+    }
+
+    public static int getQuantity(String[] lines) throws IllegalArgumentException {
+        return QuantityUtil.parseQuantity(getQuantityLine(lines));
+    }
+
+    public static String getPrice(Sign sign) {
+        return strip(StringUtil.stripColourCodes(sign.getLine(2)));
+    }
+
+    public static String getItem(Sign sign) {
+        return getItem(sign.getLines());
+    }
+
+    public static String getItem(String[] lines) {
+        return lines.length > 3 ? strip(StringUtil.stripColourCodes(lines[3])) : "";
+    }
+
+    private static String strip(String string) {
+        if (string == null)
+            return null;
+        StringBuilder stripped = new StringBuilder();
+        StringBuilder cachedWhitespace = new StringBuilder();
+        for (int codePoint : string.codePoints().toArray()) {
+            if (!Character.isWhitespace(codePoint)) {
+                if (cachedWhitespace.length() > 0) {
+                    stripped.append(cachedWhitespace);
+                    cachedWhitespace = new StringBuilder();
+                }
+                stripped.appendCodePoint(codePoint);
+            } else if (stripped.length() > 0) {
+                cachedWhitespace.appendCodePoint(codePoint);
+            }
+        }
+        return stripped.toString();
+    }
 
 }
